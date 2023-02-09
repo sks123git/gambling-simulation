bb8b70002f4c6f024ff0721247fe74bdd8f771ba
subzero <shubham8111994@gmail.com>
2023-02-09 11:51:40 +0530

    added gambler stake and bet per day and game

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,4 +1,6 @@
 public class GamblingSimulation {
+    public static final int STAKE_PER_DAY = 100;
+    public static final int BET_PER_GAME = 1;
     public static void main(String[] args) {
         System.out.println("hello world");
     }
