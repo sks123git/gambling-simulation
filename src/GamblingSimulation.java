3539945ef2740ea3cfe91075580dc97848757828
subzero <shubham8111994@gmail.com>
2023-02-09 15:11:16 +0530

    added gambler will play until win or loss equals 50%

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,21 +1,22 @@
 public class GamblingSimulation {
-    public static final int STAKE_PER_DAY = 100;
-    public static final int BET_PER_GAME = 1;
+    public static int STAKE_PER_DAY = 100;
+    public static int BET_PER_GAME = 1;
 
-    public int winLoose(){
-        return (int)(Math.floor(Math.random() * 2) +1);
+    public int winLoose() {
+        return (int) (Math.floor(Math.random() * 2) + 1);
     }
-    public static void main(String[] args) {
-        GamblingSimulation gamble= new GamblingSimulation();
-        int winLoose = gamble.winLoose();
-        int gamblerAmount = 0;
-        if(winLoose==1){
-            System.out.println(winLoose + " You win the bet");
-            gamblerAmount++;
-        }else {
-            System.out.println(winLoose + "You loose the bet");
-            gamblerAmount--;
-        }
 
+    public static void main(String[] args) {
+        GamblingSimulation gamble = new GamblingSimulation();
+        int winLoose;
+        while (STAKE_PER_DAY<=0 || STAKE_PER_DAY>=50) {
+            winLoose = gamble.winLoose();
+            if (winLoose == 1) {
+                STAKE_PER_DAY+=BET_PER_GAME;
+            } else if (winLoose == 2) {
+                STAKE_PER_DAY-=BET_PER_GAME;
+            }
+        }
+        System.out.println(STAKE_PER_DAY);
     }
 }
