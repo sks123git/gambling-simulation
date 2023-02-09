110104f5913d890410f6b4b63f0ce8b02362f407
subzero <shubham8111994@gmail.com>
2023-02-09 13:46:46 +0530

    added gambler bets either win or loose

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,7 +1,21 @@
 public class GamblingSimulation {
     public static final int STAKE_PER_DAY = 100;
     public static final int BET_PER_GAME = 1;
+
+    public int winLoose(){
+        return (int)(Math.floor(Math.random() * 2) +1);
+    }
     public static void main(String[] args) {
-        System.out.println("hello world");
+        GamblingSimulation gamble= new GamblingSimulation();
+        int winLoose = gamble.winLoose();
+        int gamblerAmount = 0;
+        if(winLoose==1){
+            System.out.println(winLoose + " You win the bet");
+            gamblerAmount++;
+        }else {
+            System.out.println(winLoose + "You loose the bet");
+            gamblerAmount--;
+        }
+
     }
 }
