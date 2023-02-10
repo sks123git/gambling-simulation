a1b20040bcfff00d1787b21a2cc73171d7b19ae6
subzero <shubham8111994@gmail.com>
2023-02-10 17:18:13 +0530

    Added won or lost in a day

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -1,22 +1,27 @@
 public class GamblingSimulation {
     public static int STAKE_PER_DAY = 100;
     public static int BET_PER_GAME = 1;
-
-    public int winLoose() {
-        return (int) (Math.floor(Math.random() * 2) + 1);
+    public double winLoose() {
+        return Math.random();
     }
-
     public static void main(String[] args) {
         GamblingSimulation gamble = new GamblingSimulation();
-        int winLoose;
-        while (STAKE_PER_DAY<=0 || STAKE_PER_DAY>=50) {
-            winLoose = gamble.winLoose();
-            if (winLoose == 1) {
-                STAKE_PER_DAY+=BET_PER_GAME;
-            } else if (winLoose == 2) {
-                STAKE_PER_DAY-=BET_PER_GAME;
-            }
-        }
-        System.out.println(STAKE_PER_DAY);
+        double winLoose;
+        int totalDays = 20;
+        int endValue=0;
+        int win=0,loss=0;
+            int amount = STAKE_PER_DAY;
+            while (totalDays>endValue) {
+                winLoose = gamble.winLoose();
+                System.out.println("win or loose : " + winLoose);
+                if (winLoose < 0.5) {
+                    amount++;
+
+                } else {
+                    amount--;
+
+                }
+                totalDays--;
+        }System.out.println("Amount = " + amount);
     }
 }
