7b2912798dc91df37e98c68ffeca0f8fcdd9312c
subzero <shubham8111994@gmail.com>
2023-02-12 11:25:34 +0530

    updated feature lucist day won maximum and loose maximum

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -7,29 +7,37 @@ public class GamblingSimulation {
     public static void main(String[] args) {
         GamblingSimulation gamble = new GamblingSimulation();
         double winLoose;
-        int totalDays = 20, totalWin=0, totalloss=0;
-        int endValue=0;
-        int win=0,loss=0;
-            int amount = STAKE_PER_DAY;
-            while (totalDays>endValue) {
+        int totalDays = 20, totalWin = 0, totalloss = 0;
+        int endValue = 0;
+        int win = 0, loss = 0;
+        int maximum = 0, minimum = 0, playAgain = 0;
+        int amount = STAKE_PER_DAY;
+        while (playAgain == 0) {
+            while (totalDays > endValue) {
                 winLoose = gamble.winLoose();
                 System.out.println("win or loose : " + winLoose);
                 if (winLoose < 0.5) {
                     amount++;
                     win++;
-                    totalWin+=amount;
+                    totalWin += amount;
+                    System.out.println("win");
                 } else {
                     amount--;
                     loss++;
-                    totalloss+=amount;
+                    totalloss += amount;
+                    System.out.println("loss");
                 }
                 totalDays--;
-        }System.out.println("Amount = " + amount);
-        System.out.println("Wins = " + win + "  Loss = " + loss);
-        if(win>loss){
-        System.out.println("won by : " + (totalWin-totalloss));}
-        else {
-            System.out.println("Lost by : " + (totalloss-totalWin));
+            }
+            System.out.println("Amount = " + amount);
+            System.out.println("Wins = " + win + "  Loss = " + loss);
+            if (win > loss) {
+                System.out.println("won by : " + (totalWin - totalloss));
+            } else {
+                System.out.println("Lost by : " + (totalloss - totalWin));
+            }
+            System.out.println("Maximum amount won: " + totalWin + " on day " + win);
+            System.out.println("Minimum amount loss: " + totalloss + " on day " + loss);
         }
     }
 }
